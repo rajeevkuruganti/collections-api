c3f36d5342a02fd80dfbd781f807e794445463e5
rkkotlin <rk-kotlin@4circlesllc.com>
2020-11-21 16:16:21 -0500

    added controller to application

--- collectibles/src/main/kotlin/com/circlesllc/collectibles/CollecitiblesApplication.kt
+++ collectibles/src/main/kotlin/com/circlesllc/collectibles/CollecitiblesApplication.kt
@@ -1,13 +1,17 @@
-package com.circlesllc.collecitibles
+package com.circlesllc.collectibles
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.transaction.annotation.EnableTransactionManagement
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 @EnableTransactionManagement
-class CollecitiblesApplication
+class CollectiblesApplication
 
 fun main(args: Array<String>) {
-	runApplication<CollecitiblesApplication>(*args)
+    runApplication<CollectiblesApplication>(*args)
+
 }
+
