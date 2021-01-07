7a02cd9962b8dd2cc3c756d116e0aef5e2aa7d8c
rkkotlin <rk-kotlin@4circlesllc.com>
2021-01-06 21:31:56 -0500

    renamed table from collection to collection_group

--- collectibles/src/main/resources/db/migration/R__insert_recs_collectionTable.sql
+++ collectibles/src/main/resources/db/migration/R__insert_recs_collectionTable.sql
@@ -1,5 +1,5 @@
-truncate table collectionGroup;
-insert into collectionGroup (id,name, collectionObj) values ( 231,'Movies', 'my DVDs');
-insert into collectionGroup (id,name, collectionObj) values ( 12,'Hindi Movies', 'my DVDs');
-insert into collectionGroup (id,name, collectionObj) values ( 13,'Hindi Songs', 'my CDs');
-insert into collectionGroup (id,name, collectionObj) values ( 984,'Carnatic Songs', 'my CDs');
+truncate table collection_Group;
+insert into collection_Group (id,name, collectionObj) values ( 231,'Movies', 'my DVDs');
+insert into collection_Group (id,name, collectionObj) values ( 12,'Hindi Movies', 'my DVDs');
+insert into collection_Group (id,name, collectionObj) values ( 13,'Hindi Songs', 'my CDs');
+insert into collection_Group (id,name, collectionObj) values ( 984,'Carnatic Songs', 'my CDs');
