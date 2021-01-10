d54220c64581e15d115232d6d8a26cac980bb3d9
rkkotlin <rk-kotlin@4circlesllc.com>
2021-01-09 23:13:05 -0500

    delete project not to be pushed

--- Mocktests/src/main/java/com/fourcircles/learn/mock/service/HelloService.java
+++
@@ -1,8 +0,0 @@
-package com.fourcircles.learn.mock.service;
-
-public class HelloService {
-    public String sayHello(String name) {
-        return "Hello " + name;
-//        return null;
-    }
-}
