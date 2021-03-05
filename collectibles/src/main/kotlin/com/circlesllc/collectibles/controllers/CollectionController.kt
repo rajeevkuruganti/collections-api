22ac424879992ebc157f9e074adf4693a3d05adf
rkkotlin <rk-kotlin@4circlesllc.com>
2021-03-05 17:00:19 -0500

    Added CrossOrigin for UI to call API

--- collectibles/src/main/kotlin/com/circlesllc/collectibles/controllers/CollectionController.kt
+++ collectibles/src/main/kotlin/com/circlesllc/collectibles/controllers/CollectionController.kt
@@ -2,12 +2,14 @@ package com.circlesllc.collectibles.controllers
 
 import com.circlesllc.collectibles.entities.CollectionGroup
 import com.circlesllc.collectibles.repository.CollectionGroupRepo
+import netscape.javascript.JSObject
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/collection")
 class CollectionController() {
@@ -24,7 +26,11 @@ class CollectionController() {
     fun getInfo(): String {
         return "This is the Collectible Application. In this application, you can store your collections and organize them. "
     }
-
+//    @GetMapping("/csAll")
+//    fun getAllCS(): List<JSObject> {
+//
+//        return collectionGroupRepo.findAll()
+//    }
     @GetMapping("/cs")
     fun getAll(): MutableIterable<CollectionGroup> {
 
