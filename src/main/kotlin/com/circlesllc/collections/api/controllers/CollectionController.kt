e0318831ec453f8ed3fff25bf90d4b80982a0b68
Rajeev Kuruganti <rkuruganti@4circlesllc.com>
2021-03-22 21:11:48 -0400

    created a new jsonb column

--- src/main/kotlin/com/circlesllc/collections/api/controllers/CollectionController.kt
+++ src/main/kotlin/com/circlesllc/collections/api/controllers/CollectionController.kt
@@ -2,11 +2,9 @@ package com.circlesllc.collections.api.controllers
 
 import com.circlesllc.collections.api.entities.CollectionGroup
 import com.circlesllc.collections.api.repository.CollectionGroupRepo
+import org.apache.tomcat.util.json.JSONParser
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @CrossOrigin("*")
@@ -33,6 +31,21 @@ class CollectionController() {
         return collectionGroupRepo.findAll()
     }
 
+    @PostMapping("/cs1")
+    fun saveNewItem(@RequestBody collectionGroup: CollectionGroup): CollectionGroup {
+//        var collectionGroup: CollectionGroup(23434,"Classics" ,"tobeDeleted",item)
+//        collectionGroup.itemContents = item
+//        collectionGroup.name = "Classics"
+//        collectionGroup.collectionobj = "to be deleted"
+        println(collectionGroup.itemcontents)
+        println(collectionGroup.name)
+//        val someJson = """{"name":"Rajeev","title":"Architect"}"""
+//        collectionGroup.itemcontents=  someJson
+        println(collectionGroup.name)
+        println("did you make it here???")
+        return  collectionGroupRepo.save(collectionGroup)
+    }
+
     @GetMapping("/cs/one")
     fun getOne(): Optional<CollectionGroup> {
         return collectionGroupRepo.findById(13)
@@ -40,3 +53,5 @@ class CollectionController() {
     }
 
 }
+
+
--- src/main/kotlin/com/circlesllc/collections/api/entities/CollectionGroup.kt
+++ src/main/kotlin/com/circlesllc/collections/api/entities/CollectionGroup.kt
@@ -1,8 +1,12 @@
 package com.circlesllc.collections.api.entities
 
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType
 import lombok.AllArgsConstructor
 import lombok.Data
 import lombok.NoArgsConstructor
+import org.hibernate.annotations.Type
+import org.hibernate.annotations.TypeDef
+
 import javax.persistence.*
 
 @Entity
@@ -10,14 +14,16 @@ import javax.persistence.*
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "collection_group")
+@TypeDef(name="jsonb", typeClass = JsonBinaryType::class)
 data class CollectionGroup(
         @Id
         @GeneratedValue(strategy = GenerationType.AUTO)
         val id: Long,
         @Column
         val name: String,
-        val collectionobj: String
-
+        val collectionobj: String,
+        @Type(type="jsonb")
+        var itemcontents: String
 
 ) {
 
