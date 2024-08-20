4073d56e2e0658a4060f1d1301d103898aaae195
rajeev kotlin <74804666+rkkotlin@users.noreply.github.com>
2024-08-20 16:48:42 -0400

    uypdated to JDK 17 and also libs are ready for Springboot3.3.2 ..

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,17 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
+	kotlin("jvm") version "1.9.24"
+	kotlin("plugin.spring") version "1.9.24"
 	id("org.springframework.boot") version "2.7.0"
+//	id("org.springframework.boot") version "3.3.2"
+	kotlin("plugin.jpa") version "1.9.24"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	id ("org.flywaydb.flyway") version "7.2.1"
-	kotlin("jvm") version "1.6.0"
-	kotlin("plugin.spring") version "1.4.10"
-	kotlin("plugin.jpa") version "1.4.10"
-
 }
 
 group = "com.circlesllc.collections.api"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 configurations {
 	compileOnly {
@@ -32,27 +31,31 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.vladmihalcea:hibernate-types-52:2.4.4")
     implementation("org.junit.jupiter:junit-jupiter:5.8.1")
-	implementation("org.junit.jupiter:junit-jupiter:5.8.1")
-//	implementation("io.minio.s3")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
-
-// https://mvnrepository.com/artifact/org.flywaydb/flyway-gradle-plugin
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	runtimeOnly ("org.flywaydb:flyway-core")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-//	testImplementation ("io.kotlintest:kotlintest-runner-junit5:3.3.2")
 	testImplementation ("io.mockk:mockk:1.9.3.kotlin12")
+	runtimeOnly("org.springframework.boot:spring-boot-properties-migrator")
 }
-
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+	tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
+		kotlinOptions {
+			jvmTarget = "17"
+		}
 	}
-}
+		tasks.withType<Test> {
+			useJUnitPlatform()
+		}
+		kotlin {
+			compilerOptions {
+				freeCompilerArgs.addAll("-Xjsr305=strict")
+			}
+		}
