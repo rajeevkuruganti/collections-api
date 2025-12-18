b5c9fc65a501bff900f7646a558f30c310e7dfad
Rajeev Kuruganti <rkuruganti@4circlesllc.com>
2025-12-18 12:36:20 -0500

    updated minio access key and secret key

---
+++ src/main/kotlin/com/circlesllc/collections/api/controllers/LoginController.kt
@@ -0,0 +1,24 @@
+package com.circlesllc.collections.api.controllers
+
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
+
+
+@RestController("/")
+@CrossOrigin("*")
+//@RequestMapping("/")
+class LoginController {
+
+    @GetMapping
+    fun showHome(): ModelAndView {
+        var mav: ModelAndView? = null
+        mav = ModelAndView("login")
+        mav.addObject("login.html")
+        return mav
+    }
+
+}
