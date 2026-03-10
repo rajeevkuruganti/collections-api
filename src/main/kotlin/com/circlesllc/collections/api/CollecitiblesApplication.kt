07a15fe685a33f8cad6059b9ce40920009b5906c
Rajeev Kuruganti <rkuruganti@4circlesllc.com>
2026-03-10 15:18:45 -0400

    using multiple URLs for CORS as property value . Updated the YAML

--- src/main/kotlin/com/circlesllc/collections/api/CollecitiblesApplication.kt
+++ src/main/kotlin/com/circlesllc/collections/api/CollecitiblesApplication.kt
@@ -1,39 +1,44 @@
 package com.circlesllc.collections.api
 
+
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import java.util.Arrays
 
 
 @SpringBootApplication
 @EnableTransactionManagement
+
 class CollectiblesApplication
 
 fun main(args: Array<String>) {
     runApplication<CollectiblesApplication>(*args)
 
 }
-
+@Component
+@ConfigurationProperties(prefix = "cors")
+data class CorsProps(
+    var allowedOrigins: List<String> = emptyList()
+)
 @Configuration
-class CorsConfig {
-    @Bean
-    fun corsFilter(): CorsFilter {
-        val source = UrlBasedCorsConfigurationSource()
-        val config = CorsConfiguration()
-        config.allowCredentials = true
-        config.addAllowedOrigin("http://localhost:5173") // Your React dev server
-        config.addAllowedOrigin("http://localhost:3000") // Alternative React port
-        config.addAllowedHeader("*")
-        config.addAllowedMethod("*") // Allows GET, POST, PUT, DELETE, etc.
-        source.registerCorsConfiguration("/**", config)
-        return CorsFilter(source)
+class CorsConfig(
+    private val corsProps: CorsProps
+) : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(*corsProps.allowedOrigins.toTypedArray())
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
     }
 }
