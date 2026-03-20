f94f1d3f82a02759e84ab70127a951b019fea4fd
Rajeev Kuruganti <rkuruganti@4circlesllc.com>
2026-03-20 06:40:21 -0400

    added in comments for username/password

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
 	kotlin("jvm") version "1.9.24"
 	kotlin("plugin.spring") version "1.9.24"
-//	id("org.springframework.boot") version "2.7.0"
 	id("org.springframework.boot") version "3.3.2"
 	kotlin("plugin.jpa") version "1.9.24"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
